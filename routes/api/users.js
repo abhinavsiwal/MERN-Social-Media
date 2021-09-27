c61e37880b73515792920a3d25d17f8044e58901
abhinavsiwal <abhinavsiwal@gmail.com>
2021-09-27 14:49:16 +0530

    Adding User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-//@route  GET api/user
-//@desc Test Route
+//@route  POST api/user
+//@desc Register User
 //@access Public
 
-router.get('/',(req,res)=>{
-    res.send('User Route');
-})
+router.post(
+  "/",
+  [
+    check("name", "Name is Required").not().isEmpty(),
+    check("email", "Please include a valid email.").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters."
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    console.log(req.body);
 
-module.exports=router;
+
+
+    res.send("User Route");
+  }
+);
+
+module.exports = router;
