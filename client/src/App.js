1bc73b8dab30ce6beb0027e8b0354ab154d1c7ee
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-02 21:10:37 +0530

    React Setup Basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React,{Fragment} from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
@@ -11,6 +13,16 @@ function App() {
       <Route path="/" exact>
       <Landing />
       </Route>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </section>
     </Fragment>
   );
 }
